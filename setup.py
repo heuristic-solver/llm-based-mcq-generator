9215786eed0be2a28e61b291fc03ca5f88607f49
ShadoWalkeR1123 <89806848+ShadoWalkeR1123@users.noreply.github.com>
2025-05-28 09:21:50 +0300

    new commit

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version = '0.0.1',
     author = 'Joel AJ.',
     author_email= 'joelalexjohn9@gmail.com',
-    install_requires = ["langchain","os","langchain_google_genai","streamlit","PyPDF2"]
+    install_requires = ["langchain","os","langchain_google_genai","streamlit","PyPDF2"],
     packages = find_packages()
 )
