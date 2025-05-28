9b7f82efc48f5fe7b546ca51ad206b1b9a36367e
ShadoWalkeR1123 <89806848+ShadoWalkeR1123@users.noreply.github.com>
2025-05-28 09:20:16 +0300

    second

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from setuptools import find_packages, setup 
+
+setup(
+    name = 'mcqgenerator',
+    version = '0.0.1',
+    author = 'Joel AJ.',
+    author_email= 'joelalexjohn9@gmail.com',
+    install_requires = ["langchain","os","langchain_google_genai","streamlit","PyPDF2"]
+    packages = find_packages()
+)
